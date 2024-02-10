e0e7ea3c929bb5d77286164100b4019fb8f6b0c8
harshadhatagale <harshadhatagale6@gmail.com>
2024-02-10 14:54:07 +0530

    error resoleve

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,5 @@
 import "@/styles/globals.css";
 import Layout from "@/components/Layout";
-import { Main } from "next/document";
 export default function App({ Component, pageProps }) {
   return (
     <main className="dark:bg-slate-800 h-lvh dark:text-white">
